7af8dba9400c6eba8dcf74dbcb7bfd23798cfec6
deku450harsh <gargn9039@gmail.com>
2023-12-20 21:55:57 +0530

    queueUsingstack

--- bitsandbytes/Q3.c
+++ bitsandbytes/Q3.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 
-int sign(int x) {
-    return (!!x)|(x>>31);
-}
 
-int main() {
-    int x = 25;
-    int result = sign(x);
-    
-    printf("sign(%d) = %d\n", x, result);
 
-    return 0;
+int main(){
+	int num,k;
+	scanf("%d", &num);
+	scanf("%d",&k);    
+
+	int sum=0;
+
+	int arr[num];
+	for(int i=0;i<num;i++){
+		scanf("%d",&arr[i]);
+	}
+	
+	int front = 0;
+	int rear = num-1;
+
+	while(front<rear && k>0){
+		if(arr[front]>=arr[rear]){
+			sum+=arr[front++];
+			k--;
+		}else{
+			break;
+		}
+	}
+
+	while(front<rear && k>0){
+		sum+=arr[rear--];
+		k--;
+	}
+
+	
+	printf("%d",sum);
+	return 0;
 }
