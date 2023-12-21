5037f1dd1f4d33db153f82b0da44c562b29a5214
deku450harsh <gargn9039@gmail.com>
2023-12-21 11:49:18 +0530

    bits

--- bitsandbytes/Q3.c
+++ bitsandbytes/Q3.c
@@ -1,37 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 
 
+int sign(int n) {
+    return (((1 << 30) | n) >> 30) & (~(!n));
+}
 
 int main(){
-	int num,k;
-	scanf("%d", &num);
-	scanf("%d",&k);    
-
-	int sum=0;
-
-	int arr[num];
-	for(int i=0;i<num;i++){
-		scanf("%d",&arr[i]);
-	}
-	
-	int front = 0;
-	int rear = num-1;
-
-	while(front<rear && k>0){
-		if(arr[front]>=arr[rear]){
-			sum+=arr[front++];
-			k--;
-		}else{
-			break;
-		}
-	}
-
-	while(front<rear && k>0){
-		sum+=arr[rear--];
-		k--;
-	}
-
-	
-	printf("%d",sum);
+	sign(2);
 	return 0;
 }
